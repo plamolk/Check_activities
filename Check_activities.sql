a5901a5f9e376c5aa5e6e15e9402a98114948c68
veerakit <nongvigo1234@gmmail.com>
2026-02-28 13:40:15 +0700

    sql

--- Check_activities.sql
+++ Check_activities.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Feb 27, 2026 at 08:08 AM
--- Server version: 9.5.0
+-- Generation Time: Feb 28, 2026 at 06:38 AM
+-- Server version: 9.6.0
 -- PHP Version: 8.3.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -20,79 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `Check_activities`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `classroom`
---
-
-CREATE TABLE `classroom` (
-  `classroom_id` int NOT NULL,
-  `classroom_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `classroom`
---
-
-INSERT INTO `classroom` (`classroom_id`, `classroom_name`) VALUES
-(1, '1'),
-(2, '2'),
-(3, '3'),
-(4, '4'),
-(5, '5'),
-(6, '6');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `department`
---
-
-CREATE TABLE `department` (
-  `department_id` int NOT NULL,
-  `department_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `grade`
---
-
-CREATE TABLE `grade` (
-  `grade_id` int NOT NULL,
-  `grade_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `grade`
---
-
-INSERT INTO `grade` (`grade_id`, `grade_name`) VALUES
-(1, '1'),
-(2, '2'),
-(3, '3');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `level`
---
-
-CREATE TABLE `level` (
-  `level_id` int NOT NULL,
-  `level_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `level`
---
-
-INSERT INTO `level` (`level_id`, `level_name`) VALUES
-(1, 'ปวช.'),
-(2, 'ปวส.');
 
 -- --------------------------------------------------------
 
@@ -121,43 +48,23 @@ INSERT INTO `role` (`role_id`, `role_name`) VALUES
 --
 
 CREATE TABLE `user` (
-  `user_id` int NOT NULL,
-  `user_code` varchar(255) DEFAULT NULL,
-  `user_fullname` varchar(255) DEFAULT NULL,
-  `role_id` int DEFAULT NULL,
-  `department_id` int DEFAULT NULL,
-  `grade_id` int DEFAULT NULL,
-  `level_id` int DEFAULT NULL,
-  `classroom_id` int DEFAULT NULL
+  `user_id` varchar(20) NOT NULL,
+  `user_rfid` varchar(255) DEFAULT NULL,
+  `user_prefix` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `user_group_code` varchar(255) DEFAULT NULL,
+  `user_group_name` varchar(255) DEFAULT NULL,
+  `user_department` varchar(255) NOT NULL,
+  `role_id` int NOT NULL,
+  `admin_user` varchar(255) DEFAULT NULL,
+  `admin_password` varchar(255) DEFAULT NULL,
+  `admin_is` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `classroom`
---
-ALTER TABLE `classroom`
-  ADD PRIMARY KEY (`classroom_id`);
-
---
--- Indexes for table `department`
---
-ALTER TABLE `department`
-  ADD PRIMARY KEY (`department_id`);
-
---
--- Indexes for table `grade`
---
-ALTER TABLE `grade`
-  ADD PRIMARY KEY (`grade_id`);
-
---
--- Indexes for table `level`
---
-ALTER TABLE `level`
-  ADD PRIMARY KEY (`level_id`);
 
 --
 -- Indexes for table `role`
@@ -170,59 +77,17 @@ ALTER TABLE `role`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`),
-  ADD KEY `role_id` (`role_id`),
-  ADD KEY `department_id` (`department_id`,`grade_id`,`level_id`,`classroom_id`),
-  ADD KEY `classroom_id` (`classroom_id`),
-  ADD KEY `level_id` (`level_id`),
-  ADD KEY `grade_id` (`grade_id`);
+  ADD KEY `role_id` (`role_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `classroom`
---
-ALTER TABLE `classroom`
-  MODIFY `classroom_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `department`
---
-ALTER TABLE `department`
-  MODIFY `department_id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `grade`
---
-ALTER TABLE `grade`
-  MODIFY `grade_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `level`
---
-ALTER TABLE `level`
-  MODIFY `level_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
   MODIFY `role_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`classroom_id`) REFERENCES `classroom` (`classroom_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_ibfk_3` FOREIGN KEY (`level_id`) REFERENCES `level` (`level_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_ibfk_4` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_ibfk_5` FOREIGN KEY (`grade_id`) REFERENCES `grade` (`grade_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
