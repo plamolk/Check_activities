64e010be45851ba12a2a9e208caa55a3a072f10d
veerakit <nongvigo1234@gmail.com>
2026-03-24 17:46:20 +0700

    กิจกกรมเกือบเสร้จ

--- backend/src/routes/activity.js
+++ backend/src/routes/activity.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const multer = require('multer');
 const { verifyToken } = require('../middlewares/auth');
 const { requireAdmin } = require('../middlewares/role');
-const { createActivity, getActivities, createActivityDraft } = require('../controllers/activity');
+const { createActivity, getActivities, createActivityDraft, getTeachers, confirmActivityTeachers, getActivityTeachers, updateActivity, getAllActivities } = require('../controllers/activity');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'src/uploads/person');
+        cb(null, 'src/uploads/activity');
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname);
@@ -21,5 +21,10 @@ const upload = multer({
 
 router.post('/add/activity', verifyToken, requireAdmin, upload.single('poster'), createActivity);
 router.get('/select/activity_draft', verifyToken, getActivities);
+router.get('/select/activity_all', verifyToken, requireAdmin, getAllActivities);
 router.post('/add/activity_draft', verifyToken, requireAdmin, createActivityDraft);
+router.post('/edit/activity/:id', verifyToken, requireAdmin, upload.single('poster'), updateActivity);
+router.post('/confirm/activity_draft/:id', verifyToken, requireAdmin, confirmActivityTeachers);
+router.get('/select/teachers', verifyToken, requireAdmin, getTeachers);
+router.get('/select/activity/:id/teachers', verifyToken, requireAdmin, getActivityTeachers);
 module.exports = router;
